c48994ecd7a61a6d1a41b18c0b09b438b3dbb852
travisburns <travisjb0@gmail.com>
2021-05-12 17:31:04 -0700

    Made Changes to Javascript

--- weather.js
+++ weather.js
@@ -1,18 +1,45 @@
-const key = `52276fcc4e67e7c6e26af4dba83aae09`;
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name');
+var description = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+var feels = document.querySelector('.feels-like');
+var humid = document.querySelector('.humid');
+var weather = document.querySelector('.weather');
+var tempmin = document.querySelector('.tempmin');
+var tempmax = document.querySelector('.tempmax');
+var country = document.querySelector('.country');
 
 
 
-const requestCity = async (city) => {
-    const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
-    const query = `?q=${city}&appid=${key}`;
+button.addEventListener('click',function(){
 
-    //make fetch call (promise call)
-    const response = await fetch(baseURL+query);
+  
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=52276fcc4e67e7c6e26af4dba83aae09')
+    
 
-
-    //promise data 
-    const data = await response.json();
-    return data;
-}
+    .then(response => response.json())
 
 
+
+.then(data => {
+    
+    var nameValue = data['name'][''];
+    var tempValue = data['main']['temp'];
+    var descValue = data['weather']['0']['description'];
+    var humidValue = data['main']['humidity'];
+    var tempminValue = data['main']['temp_min'];
+    var tempmaxValue = data['main']['temp_max'];
+    var countryValue = data['sys']['country'];
+    name.innerHTML = nameValue;
+    temp.innerHTML = parseInt((tempValue - 273) *1.8 + 32 ) + '\u00B0' + "F";
+    weather.innerHTML = descValue;
+    humid.innerHTML = humidValue + "%";
+    tempmin.innerHTML = parseInt((tempminValue - 273) *1.8 + 32) + '\u00B0' + "F" ;
+    tempmax.innerHTML = parseInt((tempmaxValue - 273) *1.8 + 32) + '\u00B0' + "F"; 
+    country.innerHTML = countryValue;
+
+
+}) 
+  
+})
